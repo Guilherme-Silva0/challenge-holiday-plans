45eee8af7ced7240731b500f6fd2408e36a8bb92
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-12 11:30:27 -0300

    :truck: chore: move register and login to auth

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -4,19 +4,26 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { CardContent, Card } from "@/components/ui/card";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="space-y-4">
         <div className="space-y-2">
           <div className="space-y-2 text-center">
-            <h1 className="text-3xl font-bold">Welcome Back</h1>
+            <h1 className="text-3xl font-bold">Create an account</h1>
             <p className="text-gray-500 dark:text-gray-400">
-              Enter your email below to login to your account
+              Already have an account?{" "}
+              <Link className="underline" href="/auth/login">
+                Sign in
+              </Link>
             </p>
           </div>
           <Card>
             <CardContent className="space-y-4 py-2">
+              <div className="space-y-2">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" required />
+              </div>
               <div className="space-y-2">
                 <Label htmlFor="email">Email</Label>
                 <Input
@@ -27,26 +34,16 @@ export default function Login() {
                 />
               </div>
               <div className="space-y-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <Link
-                    className="ml-auto inline-block text-sm underline"
-                    href="#"
-                  >
-                    Forgot your password?
-                  </Link>
-                </div>
+                <Label htmlFor="password">Password</Label>
                 <Input id="password" required type="password" />
               </div>
-              <Button className="w-full">Login</Button>
+              <div className="space-y-2">
+                <Label htmlFor="confirm-password">Confirm Password</Label>
+                <Input id="confirm-password" required type="password" />
+              </div>
+              <Button className="w-full">Sign up</Button>
             </CardContent>
           </Card>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link className="underline" href="/register">
-              Sign up
-            </Link>
-          </div>
         </div>
       </div>
     </div>
