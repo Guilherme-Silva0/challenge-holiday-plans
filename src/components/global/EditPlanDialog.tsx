0f55e30d34d81e866b83f822d0dd0a7328017c88
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-14 23:50:20 -0300

    :sparkles: feat: Edit plan functionality

--- src/components/global/EditPlanDialog.tsx
+++ src/components/global/EditPlanDialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   DialogTrigger,
@@ -11,53 +13,152 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { CardContent } from "@/components/ui/card";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { FullPlan } from "@/types/FullPlan";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formPlanSchema, TypeFormPlan } from "@/lib/formPlanSchema";
+import { useToast } from "../ui/use-toast";
+import { useApi } from "@/hooks/useApi";
+import { Loader } from "./Loader";
 
 type Props = {
   openButtonComponent: ReactNode;
+  id: string;
+  data: FullPlan;
 };
 
-export function EditPlanDialog({ openButtonComponent }: Props) {
+export function EditPlanDialog({ openButtonComponent, id, data }: Props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+  const { updatePlan } = useApi();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TypeFormPlan>({
+    resolver: zodResolver(formPlanSchema),
+    defaultValues: {
+      title: data.title,
+      description: data.description,
+      startDate: data.startDate as any,
+      endDate: data.endDate as any,
+    },
+  });
+
+  const onSubmit = handleSubmit(async (data) => {
+    setIsLoading(true);
+
+    const { error } = await updatePlan(id, data);
+
+    if (error) {
+      setIsLoading(false);
+      if (error === "Required") {
+        toast({
+          title: "Error",
+          description: "Please fill all required fields",
+          variant: "destructive",
+        });
+        return;
+      }
+
+      toast({
+        title: "Error",
+        description: error,
+        variant: "destructive",
+      });
+      return;
+    }
+
+    window.location.reload();
+
+    setIsLoading(false);
+  });
+
   return (
     <Dialog modal>
       <DialogTrigger asChild>{openButtonComponent}</DialogTrigger>
-      <DialogContent className="w-11/12 sm:max-w-3xl">
-        <DialogHeader>
-          <DialogTitle>Edit Holiday Plan</DialogTitle>
-        </DialogHeader>
-        <CardContent className="p-0">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-            <div>
-              <Label htmlFor="destination">Destination</Label>
-              <Input id="destination" placeholder="Enter destination" />
-            </div>
+      <DialogContent className="w-11/12 rounded-2xl sm:max-w-3xl">
+        <form onSubmit={onSubmit}>
+          <DialogHeader>
+            <DialogTitle>Edit Holiday Plan</DialogTitle>
+          </DialogHeader>
+          <CardContent className="p-0">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
               <div>
-                <Label htmlFor="start-date">Start Date</Label>
+                <Label htmlFor="title">Title</Label>
                 <Input
-                  id="start-date"
-                  placeholder="Enter start date"
-                  type="date"
+                  id="title"
+                  placeholder="Enter title"
+                  {...register("title")}
+                  disabled={isLoading}
                 />
+                {errors.title?.message && (
+                  <p className="text-destructive text-sm">
+                    {errors.title?.message as string}
+                  </p>
+                )}
               </div>
-              <div>
-                <Label htmlFor="end-date">End Date</Label>
-                <Input id="end-date" placeholder="Enter end date" type="date" />
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                <div>
+                  <Label htmlFor="start-date">Start Date</Label>
+                  <Input
+                    id="start-date"
+                    placeholder="Enter start date"
+                    type="date"
+                    {...register("startDate")}
+                    disabled={isLoading}
+                  />
+                  {errors.startDate?.message && (
+                    <p className="text-destructive text-sm">
+                      {errors.startDate?.message as string}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <Label htmlFor="end-date">End Date</Label>
+                  <Input
+                    id="end-date"
+                    placeholder="Enter end date"
+                    type="date"
+                    {...register("endDate")}
+                    disabled={isLoading}
+                  />
+                  {errors.endDate?.message && (
+                    <p className="text-destructive text-sm">
+                      {errors.endDate?.message as string}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="description">Description</Label>
+                <Textarea
+                  className="min-h-[100px]"
+                  id="description"
+                  placeholder="Enter description"
+                  {...register("description")}
+                  disabled={isLoading}
+                />
+                {errors.description?.message && (
+                  <p className="text-destructive text-sm">
+                    {errors.description?.message as string}
+                  </p>
+                )}
               </div>
             </div>
-            <div className="grid gap-2">
-              <Label htmlFor="description">Description</Label>
-              <Textarea
-                className="min-h-[100px]"
-                id="description"
-                placeholder="Enter description"
-              />
-            </div>
-          </div>
-        </CardContent>
-        <DialogFooter>
-          <Button type="submit">Save Changes</Button>
-        </DialogFooter>
+          </CardContent>
+          <DialogFooter>
+            <Button disabled={isLoading} type="submit">
+              {isLoading ? (
+                <Loader size="small" color="white" />
+              ) : (
+                "Save Changes"
+              )}
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
