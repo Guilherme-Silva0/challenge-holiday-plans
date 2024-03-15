20d18085db1ce6a50ab49814db897de793bb66de
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-14 23:49:46 -0300

    :sparkles: feat: Remove dialog functionality

--- src/components/global/RemovePlanDialog.tsx
+++ src/components/global/RemovePlanDialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogContent,
@@ -9,17 +11,43 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { useApi } from "@/hooks/useApi";
+import { useToast } from "../ui/use-toast";
+import { Loader } from "./Loader";
 
 type Props = {
   openButtonComponent: ReactNode;
+  id: string;
 };
 
-export function RemovePlanDialog({ openButtonComponent }: Props) {
+export function RemovePlanDialog({ openButtonComponent, id }: Props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const { deletePlan } = useApi();
+  const { toast } = useToast();
+
+  const handleDelete = async () => {
+    setIsLoading(true);
+
+    const { error } = await deletePlan(id);
+
+    if (error) {
+      return toast({
+        title: "Error",
+        description: error,
+        variant: "destructive",
+      });
+    }
+
+    window.location.reload();
+
+    setIsLoading(false);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>{openButtonComponent}</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="max-w-[400px] rounded-2xl">
         <DialogHeader>
           <DialogTitle className="pb-4">Are you absolutely sure?</DialogTitle>
           <DialogDescription className="text-red-500">
@@ -28,7 +56,13 @@ export function RemovePlanDialog({ openButtonComponent }: Props) {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button variant="destructive">Confirm</Button>
+          <Button
+            variant="destructive"
+            onClick={handleDelete}
+            disabled={isLoading}
+          >
+            {isLoading ? <Loader size="small" color="white" /> : "Confirm"}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
